dd0cff4abf9e8ecdd23309a563e16a7192ae7483
Iannaelizabeth <ianna.elizabeth.reni2gmail.com>
2026-04-21 11:12:58 +0000

    Phase 3 - Step 2: Load index from file

--- index.c
+++ index.c
@@ -1,13 +1,28 @@
-// index.c (Commit 1)
+// index.c (Commit 2)
 
 #include "index.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define INDEX_FILE ".pes/index"
+
 int index_load(Index *index) {
-    index->count = 0;
-    index->entries = NULL;
+    FILE *f = fopen(INDEX_FILE, "rb");
+
+    if (!f) {
+        index->count = 0;
+        index->entries = NULL;
+        return 0;
+    }
+
+    fread(&index->count, sizeof(int), 1, f);
+
+    index->entries = malloc(sizeof(IndexEntry) * index->count);
+
+    fread(index->entries, sizeof(IndexEntry), index->count, f);
+
+    fclose(f);
     return 0;
 }
 
