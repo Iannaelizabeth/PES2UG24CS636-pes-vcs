6eb4fa987c1bcf466f06901847e4f4840cd34706
Iannaelizabeth <ianna.elizabeth.reni2gmail.com>
2026-04-21 11:40:35 +0000

    Phase 4 - Step 5: Final commit creation with HEAD update

--- commit.c
+++ commit.c
@@ -1,4 +1,4 @@
-// commit.c (Commit 4)
+// commit.c (Commit 5 FINAL)
 
 #include "commit.h"
 #include "tree.h"
@@ -10,7 +10,7 @@
 
 int create_commit(const char *message, ObjectID *commit_id) {
     ObjectID tree_id;
-    tree_from_index(&tree_id);
+    if (tree_from_index(&tree_id) != 0) return -1;
 
     char tree_hex[65];
     hash_to_hex(&tree_id, tree_hex);
@@ -23,9 +23,21 @@ int create_commit(const char *message, ObjectID *commit_id) {
         message
     );
 
-    object_write(OBJ_COMMIT, buffer, len, commit_id);
+    if (object_write(OBJ_COMMIT, buffer, len, commit_id) != 0) {
+        return -1;
+    }
 
-    printf("Commit object written\n");
+    // update HEAD
+    char commit_hex[65];
+    hash_to_hex(commit_id, commit_hex);
+
+    FILE *f = fopen(".pes/HEAD", "w");
+    if (f) {
+        fprintf(f, "%s\n", commit_hex);
+        fclose(f);
+    }
+
+    printf("Committed: %s\n", commit_hex);
 
     return 0;
 }
